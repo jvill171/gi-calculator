1f4dbff709ce18ac415f1b7b492c3f3337371096
jvill171 <juanvillarreal64@yahoo.com>
2021-11-14 11:25:33 -0600

    Minor changes to pages

--- src/pages/characters.vue
+++ src/pages/characters.vue
@@ -7,32 +7,39 @@
             
             <ul class="flex flex-wrap bg-gray-600 p-5 rounded-b-lg items-stretch justify-center mb-10">
                 <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-green-500" src="../assets/elements/anemo.png">
                     <img src="../assets/characters_small/aloy.png" class="bg-oneStar bg-cover rounded-t">
                     <p class="text-center">Aloy A</p>
                 </li>
-                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
-                <img src="../assets/characters_small/aloy.png" class="bg-twoStar bg-cover rounded-t">
-                <p class="text-center">Aloy B</p>
+                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg">  
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-blue-500" src="../assets/elements/hydro.png">
+                    <img src="../assets/characters_small/aloy.png" class="bg-twoStar bg-cover rounded-t">
+                    <p class="text-center">Aloy B</p>
                 </li>
-                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
-                <img src="../assets/characters_small/aloy.png" class="bg-threeStar bg-cover rounded-t">
-                <p class="text-center">Aloy C</p>
+                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg">  
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-red-500" src="../assets/elements/pyro.png">
+                    <img src="../assets/characters_small/aloy.png" class="bg-threeStar bg-cover rounded-t">
+                    <p class="text-center">Aloy C</p>
                 </li>
-                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
-                <img src="../assets/characters_small/aloy.png" class="bg-fourStar bg-cover rounded-t">
-                <p class="text-center">Aloy D</p>
+                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg">  
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-green-700" src="../assets/elements/dendro.png">
+                    <img src="../assets/characters_small/aloy.png" class="bg-fourStar bg-cover rounded-t">
+                    <p class="text-center">Aloy D</p>
                 </li>
-                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
-                <img src="../assets/characters_small/aloy.png" class="bg-fiveStar bg-cover rounded-t">
-                <p class="text-center">Aloy E</p>
+                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg">  
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-blue-300" src="../assets/elements/cryo.png">
+                    <img src="../assets/characters_small/aloy.png" class="bg-fiveStar bg-cover rounded-t">
+                    <p class="text-center">Aloy E</p>
                 </li>
-                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
-                <img src="../assets/characters_small/aloy.png" class="bg-oneStar bg-cover rounded-t">
-                <p class="text-center">Aloy A</p>
+                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg">  
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-purple-500" src="../assets/elements/electro.png">
+                    <img src="../assets/characters_small/aloy.png" class="bg-oneStar bg-cover rounded-t">
+                    <p class="text-center">Aloy A</p>
                 </li>
-                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
-                <img src="../assets/characters_small/aloy.png" class="bg-twoStar bg-cover rounded-t">
-                <p class="text-center">Aloy B</p>
+                <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg">  
+                    <img class="absolute w-8 p-1 m-1 rounded-full bg-gray-700 border-2 border-yellow-400" src="../assets/elements/geo.png">
+                    <img src="../assets/characters_small/aloy.png" class="bg-twoStar bg-cover rounded-t">
+                    <p class="text-center">Aloy B</p>
                 </li>
                 <li class="m-2 w-auto bg-gray-200 rounded drop-shadow-lg"> 
                 <img src="../assets/characters_small/aloy.png" class="bg-threeStar bg-cover rounded-t">
