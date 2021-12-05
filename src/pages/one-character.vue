409e5b5b52430ec517d3c6e08ca1a5b9582357e8
jvill171 <juanvillarreal64@yahoo.com>
2021-12-05 02:07:07 -0600

    Finished final logic an layout

--- src/pages/one-character.vue
+++ src/pages/one-character.vue
@@ -6,27 +6,24 @@ import { useLocal } from "../composable/useLocal";
 
 const route = useRoute();
 const { name } = route.params;
-//console.log(name);
 
-const { getCharacter, getBooks, getJewels, getBoss, getSpecialty } = useLocal();
+const {
+  getCharacter,
+  getBooks,
+  getJewels,
+  getBoss,
+  getSpecialty,
+  getWeekly,
+  getMob,
+} = useLocal();
 
 const thisCharacter = getCharacter(name);
 const thisBook = getBooks(name);
 const thisJewel = getJewels(name);
 const thisBoss = getBoss(name);
 const thisSpecialty = getSpecialty(name);
-
-const XPCounter = [
-  0, 1000, 1325, 1700, 2150, 2625, 3150, 3725, 4350, 5000, 5700, 6450, 7225,
-  8050, 8925, 9825, 10750, 11725, 12725, 13775, 14875, 16800, 18000, 19250,
-  20550, 21875, 23250, 24650, 26100, 27575, 29100, 30650, 32250, 33875, 35550,
-  37250, 38975, 40750, 42575, 44425, 46300, 50625, 52700, 54775, 56900, 59075,
-  61275, 63525, 65800, 68125, 70475, 76500, 79050, 81650, 84275, 86950, 89650,
-  92400, 95175, 98000, 100875, 108950, 112050, 115175, 118325, 121525, 124775,
-  128075, 131400, 134775, 138175, 148700, 152375, 156075, 159825, 163600,
-  167425, 171300, 175225, 179175, 183175, 216225, 243025, 273100, 306800,
-  344600, 386950, 434425, 487625, 547200,
-];
+const thisWeekly = getWeekly(name);
+const thisMob = getMob(name);
 
 const cTal = ref([1, 1, 1]);
 const dTal = ref([1, 1, 1]);
@@ -34,26 +31,17 @@ const cMaxTal = ref(1);
 const dMaxTal = ref(1);
 
 const level = ref([1, 1]);
-const bCount = ref([0, 0, 0]);
-const jCount = ref([0, 0, 0, 0]);
-const bmCount = ref(0);
-const sCount = ref(0);
-const mora = ref(0);
+const bCount = ref([0, 0, 0]); //Book
+const jCount = ref([0, 0, 0, 0]); //Jewel
+const bmCount = ref(0); //Boss Material
+const sCount = ref(0); //Specialty
+const mCount = ref([0, 0, 0]); //Mob
+const wCount = ref(0); //weekly boss material
+const crowns = ref(0);
+const mora = ref("0");
+const xp = ref("0");
 const ascLevel = ref([0, 0]);
 
-//non-dynamic variables
-const calcXP = () => {
-  let max = 90;
-  totalXP = 0;
-  for (let min = 1; min < max; min++) {
-    totalXP += XPCounter[min];
-  }
-  console.log(totalXP);
-  ascensionCount = totalXP;
-  return totalXP;
-};
-
-//Relies on non-dynamic variables, fix this
 const getMaxTalent = (ascensionLevel) => {
   let myTalent = 1;
   if (ascensionLevel >= 2) {
@@ -105,8 +93,6 @@ const getAscension = (oldLevel, newLevel) => {
 };
 
 const setNewLevel = (asc, cLevel) => {
-  console.log(asc);
-  console.log(cLevel);
   if ((asc == 6 && cLevel < 80) || (asc == 5 && cLevel > 80)) {
     return 80;
   } else if ((asc == 5 && cLevel < 70) || (asc == 4 && cLevel > 70)) {
@@ -122,6 +108,132 @@ const setNewLevel = (asc, cLevel) => {
   } else {
     return cLevel;
   }
+};
+
+//CHARTS FOR USE WITH METHODS
+const chartXP = [
+  0, 1000, 1325, 1700, 2150, 2625, 3150, 3725, 4350, 5000, 5700, 6450, 7225,
+  8050, 8925, 9825, 10750, 11725, 12725, 13775, 14875, 16800, 18000, 19250,
+  20550, 21875, 23250, 24650, 26100, 27575, 29100, 30650, 32250, 33875, 35550,
+  37250, 38975, 40750, 42575, 44425, 46300, 50625, 52700, 54775, 56900, 59075,
+  61275, 63525, 65800, 68125, 70475, 76500, 79050, 81650, 84275, 86950, 89650,
+  92400, 95175, 98000, 100875, 108950, 112050, 115175, 118325, 121525, 124775,
+  128075, 131400, 134775, 138175, 148700, 152375, 156075, 159825, 163600,
+  167425, 171300, 175225, 179175, 183175, 216225, 243025, 273100, 306800,
+  344600, 386950, 434425, 487625, 547200,
+];
+//chartJewels [qty, type(index)]
+const chartJewel = [
+  [1, 0],
+  [3, 1],
+  [6, 1],
+  [3, 2],
+  [6, 2],
+  [6, 3],
+];
+//chartBook [qty, type(index)], starts at Lvl 2 Tal upgrade
+const chartBook = [
+  [3, 0],
+  [2, 1],
+  [4, 1],
+  [6, 1],
+  [9, 1],
+  [4, 2],
+  [6, 2],
+  [12, 2],
+  [16, 2],
+];
+//chartAscMob [qty, type(index)]
+const chartAscMob = [
+  [3, 0],
+  [15, 0],
+  [12, 1],
+  [18, 1],
+  [12, 2],
+  [24, 2],
+];
+//chartTalMob [qty, type(index)]
+const chartTalMob = [
+  [6, 0],
+  [3, 1],
+  [4, 1],
+  [6, 1],
+  [9, 1],
+  [4, 2],
+  [6, 2],
+  [9, 2],
+  [12, 2],
+];
+const chartBoss = [0, 2, 4, 8, 12, 20];
+const chartSpecialty = [3, 10, 20, 30, 45, 60];
+const chartTalMora = [
+  0, 12500, 17500, 25000, 30000, 37500, 120000, 260000, 450000, 700000,
+];
+const jbmsGet = (cAsc, dAsc) => {
+  let j = [0, 0, 0, 0];
+  let bm = 0;
+  let s = 0;
+
+  if (cAsc < dAsc) {
+    for (; cAsc < dAsc; cAsc++) {
+      j[chartJewel[cAsc][1]] += chartJewel[cAsc][0];
+      bm += chartBoss[cAsc];
+      s += chartSpecialty[cAsc];
+    }
+  }
+  return [j, bm, s];
+};
+const mobMoraXPGet = (lvl, asc, cT, dT) => {
+  let thisXP = 0;
+  let thisMora = 0;
+  let thisMob = [0, 0, 0];
+
+  console.log(`lvl = ${lvl}\nasc=${asc}\ncT=${cT}\ndT=${dT}\n`);
+  //Level Up Cost
+  for (; lvl[0] < lvl[1]; lvl[0]++) {
+    thisXP += chartXP[lvl[0]];
+  }
+  thisMora += thisXP / 5;
+  //Ascension Cost
+  for (; asc[0] < asc[1]; asc[0]++) {
+    thisMora += 20000 * (asc[0] + 1);
+    thisMob[chartAscMob[asc[0]][1]] += chartAscMob[asc[0]][0];
+  }
+  //Talent Cost
+  for (let i = 0; i < 3; i++) {
+    for (; cT[i] < dT[i]; cT[i]++) {
+      thisMob[chartTalMob[cT[i] - 1][1]] += chartTalMob[cT[i] - 1][0];
+      thisMora += chartTalMora[cT[i]];
+    }
+  }
+  return [
+    thisMob,
+    thisMora.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    thisXP.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+  ];
+};
+const weeklyBookGet = (c, d) => {
+  let w = 0;
+  let b = [0, 0, 0];
+  let crn = 0;
+
+  //For each of the 3 talents
+  for (let i = 0; i < 3; i++) {
+    //For each Talent Level
+    for (; c[i] < d[i]; c[i]++) {
+      b[chartBook[c[i] - 1][1]] += chartBook[c[i] - 1][0];
+      if (c[i] > 5) {
+        w++;
+      }
+      if (c[i] > 7) {
+        w++;
+      }
+      if (c[i] == 9) {
+        crn++;
+      }
+    }
+  }
+  return [w, b, crn];
 };
 </script>
 
@@ -145,9 +257,30 @@ const setNewLevel = (asc, cLevel) => {
         </p>
       </div>
       <!-- Interactive Area -->
-      <div class="rounded-r-lg grid grid-rows-5 w-2/3 text-white">
+      <div
+        @click="
+          ([jCount, bmCount, sCount] = jbmsGet(ascLevel[0], ascLevel[1])),
+            ([wCount, bCount, crowns] = weeklyBookGet([...cTal], [...dTal])),
+            ([mCount, mora, xp] = mobMoraXPGet(
+              [...level],
+              [...ascLevel],
+              [...cTal],
+              [...dTal]
+            ))
+        "
+        class="rounded-r-lg grid grid-rows-5 w-2/3 text-white"
+      >
         <!-- Current Talents -->
-        <div class="bg-gray-700 text-center rounded-tr-md row-span-2 lg:pt-3 hover:scale-105">
+        <div
+          class="
+            bg-gray-700
+            text-center
+            rounded-tr-md
+            row-span-2
+            lg:pt-3
+            hover:scale-105
+          "
+        >
           <p class="underline pt-3">Talents (Current)</p>
           <div class="container flex space-x-3 px-3 py-2">
             <div
@@ -357,7 +490,15 @@ const setNewLevel = (asc, cLevel) => {
           </div>
         </div>
         <!-- Level/Ascension-->
-        <div class="bg-gray-700 px-5 rounded-br-md row-span-1 hover:scale-105 hover:shadow-lg">
+        <div
+          class="
+            bg-gray-700
+            px-5
+            rounded-br-md
+            row-span-1
+            hover:scale-105 hover:shadow-lg
+          "
+        >
           <!-- Level -->
           <p class="text-center underline pt-2">Character Level</p>
           <div class="self-center p-2 grid grid-cols-2 gap-5">
@@ -700,13 +841,176 @@ const setNewLevel = (asc, cLevel) => {
         flex flex-wrap
         content-around
         text-center
-        justify-between
+        justify-evenly
         items-center
+        bg-gray-600
+        border-2 border-black
+        rounded-lg
       "
     >
-      <!-- Books
-        :class="bookCount[index] <= 0 ? 'hidden' : ''" -->
-      <div
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-2xl
+          p-5
+          flex-wrap
+          m-3
+          border-2 border-black
+        "
+      >
+        <img class="h-20" src="/ascension/currency/mora.png" />
+        <p class="p-3">
+          Mora Needed
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+            >{{ mora }}
+          </span>
+        </p>
+      </li>
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-2xl
+          p-5
+          flex-wrap
+          m-3
+          border-2 border-black
+        "
+      >
+        <img
+          class="h-20"
+          src="/ascension/experience/adventurers-experience.png"
+        />
+        <p class="p-3">
+          Experience Needed
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+            >{{ xp }}
+          </span>
+        </p>
+      </li>
+    </ul>
+    <ul class="flex flex-wrap text-center justify-around items-center">
+      <!-- Jewels -->
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-full
+          p-5
+          flex-wrap
+          m-3
+          border-2 border-black
+        "
+        v-for="(j, name, index) in thisJewel"
+        :key="j"
+        :class="jCount[index] == 0 ? 'hidden' : ''"
+      >
+        <img class="h-20" :src="j.image" />
+        <p class="p-3">
+          {{ j.name }}
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+            >{{ jCount[index] }}
+          </span>
+        </p>
+      </li>
+      <!-- Mob Drop -->
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-full
+          p-5
+          m-3
+          border-2 border-black
+          text-sm
+        "
+        v-for="(mob, name, index) in thisMob.mob_mats"
+        :key="mob"
+        :class="mCount[index] == 0 ? 'hidden' : ''"
+      >
+        <img class="h-20" :src="mob.image" />
+        <p class="p-2">
+          {{ mob.name }}
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+          >
+            {{ mCount[index] }}
+          </span>
+        </p>
+      </li>
+      <!-- Boss -->
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-full
+          p-5
+          flex-wrap
+          m-3
+          border-2 border-black
+        "
+        :class="bmCount == 0 ? 'hidden' : ''"
+      >
+        <img class="h-20" :src="thisBoss.image" />
+        <p class="p-3">
+          {{ thisBoss.name }}
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+            >{{ bmCount }}
+          </span>
+        </p>
+      </li>
+      <!-- Books -->
+      <li
         class="
           flex
           bg-gray-700
@@ -718,6 +1022,7 @@ const setNewLevel = (asc, cLevel) => {
         "
         v-for="(b, name, index) in thisBook"
         :key="b"
+        :class="bCount[index] == 0 ? 'hidden' : ''"
       >
         <img class="h-20" :src="b.image" />
         <p class="p-2">
@@ -737,10 +1042,9 @@ const setNewLevel = (asc, cLevel) => {
             {{ bCount[index] }}
           </span>
         </p>
-        <div></div>
-      </div>
-      <!-- Jewels -->
-      <div
+      </li>
+      <!-- Specialty -->
+      <li
         class="
           flex
           bg-gray-700
@@ -750,66 +1054,7 @@ const setNewLevel = (asc, cLevel) => {
           m-3
           border-2 border-black
         "
-        v-for="(j, name, index) in thisJewel"
-        :key="j"
-      >
-        <img class="h-20" :src="j.image" />
-        <p class="p-3">
-          {{ j.name }}
-          <br />
-          <br />
-          <span
-            class="
-              bg-white
-              py-1
-              px-4
-              text-black
-              rounded-full
-              border-2 border-black
-            "
-            >{{ jCount[index] }}
-          </span>
-        </p>
-      </div>
-      <div
-        class="
-          flex
-          bg-gray-700
-          rounded-full
-          p-5
-          flex-wrap
-          m-3
-          border-2 border-black
-        "
-      >
-        <img class="h-20" :src="thisBoss.image" />
-        <p class="p-3">
-          {{ thisBoss.name }}
-          <br />
-          <br />
-          <span
-            class="
-              bg-white
-              py-1
-              px-4
-              text-black
-              rounded-full
-              border-2 border-black
-            "
-            >{{ bmCount }}
-          </span>
-        </p>
-      </div>
-      <div
-        class="
-          flex
-          bg-gray-700
-          rounded-full
-          p-5
-          flex-wrap
-          m-3
-          border-2 border-black
-        "
+        :class="sCount == 0 ? 'hidden' : ''"
       >
         <img class="h-20" :src="thisSpecialty.image" />
         <p class="p-3">
@@ -828,7 +1073,69 @@ const setNewLevel = (asc, cLevel) => {
             >{{ sCount }}
           </span>
         </p>
-      </div>
+      </li>
+      <!-- Weekly -->
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-full
+          p-5
+          flex-wrap
+          m-3
+          border-2 border-black
+        "
+        :class="wCount == 0 ? 'hidden' : ''"
+      >
+        <img class="h-20" :src="thisWeekly.image" />
+        <p class="p-3">
+          {{ thisWeekly.name }}
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+            >{{ wCount }}
+          </span>
+        </p>
+      </li>
+      <!-- Crown -->
+      <li
+        class="
+          flex
+          bg-gray-700
+          rounded-full
+          p-5
+          flex-wrap
+          m-3
+          border-2 border-black
+        "
+        :class="crowns == 0 ? 'hidden' : ''"
+      >
+        <img class="h-20" src="/ascension/talents/crown-of-insight.png" />
+        <p class="p-3">
+          Crown of Insight
+          <br />
+          <br />
+          <span
+            class="
+              bg-white
+              py-1
+              px-4
+              text-black
+              rounded-full
+              border-2 border-black
+            "
+            >{{ crowns }}
+          </span>
+        </p>
+      </li>
     </ul>
   </div>
 </template>
