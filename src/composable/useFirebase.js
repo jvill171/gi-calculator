c818652aece79a8ceb38a682e1b340b7fdee4377
jvill171 <41810008+jvill171@users.noreply.github.com>
2024-05-27 17:59:19 -0500

    Removed braces from firebaseConfig import, as they are now unnecessary

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-import { firebaseConfig } from "../firebaseConfig";
+import firebaseConfig from "../firebaseConfig";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
